5ca1bcbb7a6c35af8b652e5b25d577b174783430
karthknbs <karthike.369@gmail.com>
2021-03-25 19:50:17 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,7 +17,7 @@ public class DemoApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello hi %s!", name);
+		return String.format("Hello hi thr %s!", name);
 	}
 
 }
